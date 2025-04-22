bbe835c387e4441ee182f41b697840591d0c8543
Amit Mehta <amitmehta.aws@gmail.com>
2025-04-23 04:29:24 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>Coming Soon<h1/>\n", 200
+
+
+@app.route("/version", methods=["GET"])
+def version():
+    return "v0.0.1\n", 200
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port="5500", debug=True)
